4710e95eb292db9d4905a498d3a6e28a0fef3786
구혜연 <guhaa03@daum.net>
2025-06-19 23:43:13 +0900

    feat : 필터 버튼 컴포넌트 구현

---
+++ src/components/FilterButtons.tsx
@@ -0,0 +1,37 @@
+const categories = ["협업", "기술", "자기소개"];
+const levels = ["신입", "중고신입", "경력"];
+
+export default function FilterButtons() {
+  return (
+    <div className="flex gap-4">
+      <div className="flex flex-col gap-2 ">
+        <label htmlFor="category" className="font-semibold">
+          카테고리
+        </label>
+        <select
+          name="category"
+          id="category"
+          className="border rounded p-2 border-gray-400"
+        >
+          {categories.map((c) => (
+            <option key={c}>{c}</option>
+          ))}
+        </select>
+      </div>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="level" className="font-semibold">
+          경력
+        </label>
+        <select
+          name="level"
+          id="level"
+          className="border rounded p-2 border-gray-400"
+        >
+          {levels.map((l) => (
+            <option key={l}>{l}</option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+}
