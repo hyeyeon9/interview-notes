9e46a459244e02fc0a6c9c627f42b8648dc7407b
구혜연 <guhaa03@daum.net>
2025-06-19 22:26:21 +0900

    feat: 답변 저장용 Answer 테이블 추가 (questionId 외래키 포함)

---
+++ prisma/migrations/20250619123805_create_answer_table/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Answer" (
+    "id" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "questionId" TEXT NOT NULL,
+
+    CONSTRAINT "Answer_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Answer" ADD CONSTRAINT "Answer_questionId_fkey" FOREIGN KEY ("questionId") REFERENCES "Question"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
